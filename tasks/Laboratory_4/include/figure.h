0336136797f140e5e53ff5fb1ad48ce522a867e3
ArtemPiatnitskiy <ARTnov78095@outlook.com>
2025-11-03 18:53:59 +0300

    Create template struct

--- tasks/Laboratory_4/include/figure.h
+++ tasks/Laboratory_4/include/figure.h
@@ -1,28 +1,35 @@
 #pragma once
 #include <iostream>
 #include "point.h"
+#include <memory>
 #include <string_view>
 #include <string>
 #include <ostream>
 
-
-class Figure {
+template<Scalar T> class Figure {
     // Функция перегрузки оператора вывода. Является friend функций, чтобы иметь доступ к приватным членам классса.
     // Функция будет возвращать ссылку на поток вывода для возможности цепочки выводов.
     // Пример использования:
     // Figure fig;
     // std::cout << fig << std::endl;
-    friend std::ostream& operator<<(std::ostream& os, const Figure& figure);
+    friend std::ostream& operator<<(std::ostream& os, const Figure& figure) {
+        os << figure.description << ":\n";
+        figure.print(os);
+        return os;
+    }
     // Аналогичная функция перегрузки оператора ввода.
     // Не const, так как мы будем изменять объект figure при вводе данных.
-    friend std::istream& operator>>(std::istream& is, Figure&  figure);
+    friend std::istream& operator>>(std::istream& is, Figure& figure) {
+        figure.read(is);
+        return is;
+    };
     protected:
         // Конструкторы защищены, чтобы нельзя было создать объект базового класса напрямую.
         // Это абстрактный класс.
         // Конструктор по умолчанию.
         Figure() = default;
         // Конструктор с параметром описания фигуры.
-        Figure(std::string_view discription);
+        Figure(std::string_view descrip) : description(descrip) {};
 
         // Функции для реализации логики операторов ввода/вывода в наследниках.
 
@@ -35,7 +42,7 @@ class Figure {
         virtual ~Figure() = default;
 
         // Функция для нахождения геометрического центра фигуры.
-        virtual Point geometric_center() const = 0;
+        virtual std::unique_ptr<Point<T>> geometric_center() const = 0;
         // Функция для нахождения площади фигуры.
         virtual double square() const = 0;
         // Функция для нахождения периметра фигуры.
